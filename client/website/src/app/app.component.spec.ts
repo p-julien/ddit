dff7c0cd02837712fdde454acac2e6edd321f8b2
Pierre Julien <pierrejulienpro@gmail.com>
2022-08-03 00:16:10 +0200

    fix: tests in client apps

--- client/website/src/app/app.component.spec.ts
+++ client/website/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -12,14 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'website app is running!'
-    );
   });
 });
