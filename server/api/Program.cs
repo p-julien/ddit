ded90fbe0907104931d02d607439dc3d06ed41b9
Pierre Julien <pierrejulienpro@gmail.com>
2022-08-02 18:53:51 +0200

    feat: gestion mock dans la classe program

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,12 +1,17 @@
 using api.Services;
 using api.Services.Implementation;
+using api.Services.Mock;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Define mock as project configuration
+const bool isMock = false;
+
 // Add services to the container.
-builder.Services.AddSingleton<IRedditService, RedditService>();
+_ = isMock ? RegisterMockServices(builder) : RegisterServices(builder);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,3 +32,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+bool RegisterServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddSingleton<IRedditService, RedditService>();
+    return true;
+}
+
+bool RegisterMockServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddSingleton<IRedditService, RedditServiceMock>();
+    return true;
+}
